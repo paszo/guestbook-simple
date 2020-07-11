d05a07d9129fdff28d141e76c8bf6e8fe6b45f59
paszo <paszo2008@gmail.com>
2020-07-11 10:24:57 +0200

    Added simple ejs view

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send('hello world');
-})
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const entries = [];
+app.locals.entries = entries;
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get("/",  function (req, res) {
+    res.render("index");
+});
 
 const port = process.env.PORT || 3000;
 
